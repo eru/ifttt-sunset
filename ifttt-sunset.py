a06a8916120d478a2427c40e5434aaefbddc2cf3
Kazuki Hashimoto <eru.tndl@gmail.com>
2020-05-11 01:21:21 +0900

    :up: Added options for running from job schedulers such as CRON.

--- ifttt-sunset.py
+++ ifttt-sunset.py
@@ -8,19 +8,37 @@
 
 import argparse
 from datetime import datetime, timedelta, timezone
+import os
 import requests
 from suntime import Sun
 
 
-parser = argparse.ArgumentParser(description='This script calls the Maker event API of ifttt at sunset time.')
+ONCE_A_DAY_FILE = os.path.dirname(os.path.abspath(__file__)) + os.sep + '.last'
 
+
+parser = argparse.ArgumentParser(description='This script calls the Maker event API of ifttt at sunset time.')
 parser.add_argument('--latitude', '-lat', action='store', type=float, required=True, help='Latitude')
 parser.add_argument('--longitude', '-lng', action='store', type=float, required=True, help='Longitude')
 parser.add_argument('--event', '-e', action='store', type=str, required=True, help='IFTTT Maker Event API name')
 parser.add_argument('--key', '-k', action='store', type=str, required=True, help='IFTTT Maker Event API key')
 parser.add_argument('--add', '-a', action='store', nargs='?', type=int, help='Additional time (minutes)')
-
+parser.add_argument('--once-a-day', '-once', action='store_true', help='Runs once a day for execution from a job scheduler such as CRON')
 args = parser.parse_args()
+
+now = datetime.now(timezone.utc)
+
+if args.once_a_day and os.path.exists(ONCE_A_DAY_FILE):
+    with open(ONCE_A_DAY_FILE, 'r') as f:
+        line = f.readline()
+        try:
+            last = datetime.fromisoformat(line)
+        except:
+            print('The last execution date is an invalid value. Delete the `.last` file.')
+            exit(1)
+
+        if now.date() == last.date():
+            # Do nothing
+            exit(0)
 
 try:
     sun = Sun(args.latitude, args.longitude)
@@ -37,8 +55,6 @@ if args.add:
         exit(1)
     sunset += add_timedelta
 
-now = datetime.now(timezone.utc)
-
 if now < sunset:
     # Do nothing
     exit(0)
@@ -48,3 +64,7 @@ response = requests.post(url)
 if response.status_code != 200:
     print('Failed to post to `{url}`.'.format(url=response.url))
     exit(1)
+
+if args.once_a_day:
+    with open(ONCE_A_DAY_FILE, 'w') as f:
+        f.write(now.isoformat())
